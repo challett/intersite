7ad21016b8272d1195765a4430569779cf53a4ba
challett <connor.hallett@gmail.com>
2014-03-29 22:53:21 +0000

    Enhanced User Editing

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -4,6 +4,12 @@ class InterestsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @interest = @user.interests.create(params[:interest].permit(:name))
-    redirect_to user_path(@user)
+    redirect_to edit_user_path(@user)
+  end
+  def destroy
+    @user = User.find(params[:user_id])
+    @interest = @user.interests.find(params[:id])
+    @interest.destroy
+    redirect_to edit_user_path(@user)
   end
 end
