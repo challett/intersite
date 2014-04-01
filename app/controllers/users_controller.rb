ae2f2f42c7b5c4adad353e89786b358b4b2c7e19
challett <connor.hallett@gmail.com>
2014-04-01 01:55:16 +0000

    Working on updating interest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update(user_params) 
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
