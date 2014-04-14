8869a2d3647b891900ad78fc8ecd74f0979458f0
Connor Hallett <connor.hallett@gmail.com>
2014-04-14 00:04:35 +0000

    Added adding interests

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,4 +1,4 @@
 class Interest < ActiveRecord::Base
   has_and_belongs_to_many :users
-  validates :name,  presence: true
+  validates :name,  presence: true, uniqueness:{ case_sensitive: false }
 end
