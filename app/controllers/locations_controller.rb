fa354660bdeecc8197328e8217c1397fe3e1e053
Connor Hallett <connor.hallett@gmail.com>
2014-05-03 01:43:31 +0000

    Removed backup files

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,7 +8,7 @@ class LocationsController < ApplicationController
   end
   
   def create
-    @location = Location.new(location_params)
+    @location = Location.create(location_params)
     redirect_to locations_path
   end
   
