678f271782db8631ed15dd48b9044f373f6abf5a
Connor Hallett <connor.hallett@gmail.com>
2014-05-11 19:19:11 +0000

    started user customization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510224811) do
+ActiveRecord::Schema.define(version: 20140511180922) do
 
   create_table "interests", force: true do |t|
     t.string   "name"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20140510224811) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "picture_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
